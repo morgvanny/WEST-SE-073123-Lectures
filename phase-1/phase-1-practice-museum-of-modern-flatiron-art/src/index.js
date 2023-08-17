2333eb02281686318f6f030a1d9587b89dc1ef50
Morgan VanYperen <morgvanny@gmail.com>
2023-08-17 09:07:41 -0700

    done

--- phase-1/phase-1-practice-museum-of-modern-flatiron-art/src/index.js
+++ phase-1/phase-1-practice-museum-of-modern-flatiron-art/src/index.js
@@ -1,1 +1,47 @@
-console.log('Write your code here');
+let currentExhibit;
+
+const commentSectionDiv = document.querySelector("#comments-section");
+const ticketsSpan = document.querySelector("#tickets");
+
+const displayComment = (comment) => {
+  const commentP = document.createElement("p");
+  commentP.textContent = comment;
+  commentSectionDiv.append(commentP);
+};
+
+const displayExhibit = (exhibit) => {
+  currentExhibit = exhibit;
+  const h2Title = document.querySelector("#exhibit-title");
+  h2Title.textContent = exhibit.title;
+  const img = document.querySelector("#exhibit-image");
+  img.src = exhibit.image;
+  img.alt = exhibit.title;
+
+  ticketsSpan.textContent = exhibit.tickets_bought;
+  const descP = document.querySelector("#exhibit-description");
+  descP.textContent = exhibit.description;
+
+  exhibit.comments.forEach(displayComment);
+};
+
+fetch("http://localhost:3000/current-exhibits/1")
+  .then((resp) => resp.json())
+  .then((ex) => {
+    displayExhibit(ex);
+  });
+
+const form = document.querySelector("#comment-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const comment = form["comment-input"].value;
+  displayComment(comment);
+  form.reset();
+});
+
+document.querySelector("#buy-tickets-button").addEventListener("click", () => {
+  currentExhibit.tickets_bought += 1;
+  ticketsSpan.textContent = currentExhibit.tickets_bought;
+  // const current = parseInt(ticketsSpan.textContent);
+  // ticketsSpan.textContent = current + 1;
+});
