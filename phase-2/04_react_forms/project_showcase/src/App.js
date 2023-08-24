c1ce006a486484ca16fb4fcf622645db83c4d39a
Morgan VanYperen <morgvanny@gmail.com>
2023-08-24 09:59:10 -0700

    init forms

---
+++ phase-2/04_react_forms/project_showcase/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import ProjectForm from "./components/ProjectForm";
+import ProjectList from "./components/ProjectList";
+
+// Deliverable 2: Handle submitting the form and update state in parent 
+// using inverse data flow
+
+// - When the form is submitted:
+
+//   - Update the `projects` state located in the parent component, `App`
+//   using inverse data flow
+
+//     - Use the spread operator to return a new array with the new project included
+
+//     - Set the `projects` state to the new array value
+
+const App = () => {
+  const [projects, setProjects] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const handleClick = () => {
+    fetch("http://localhost:4000/projects")
+      .then((res) => res.json())
+      .then((projects) => setProjects(projects));
+  };
+
+  const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
+  return (
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
+      <ProjectForm />
+      <button onClick={handleClick}>Load Projects</button>
+      <ProjectList projects={projects} />
+    </div>
+  );
+};
+
+export default App;
