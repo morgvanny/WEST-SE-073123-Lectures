25dcd5b00eba44d9f0d535bdb342bb8384354836
Morgan VanYperen <morgvanny@gmail.com>
2023-08-28 09:29:15 -0700

    done

--- phase-2/05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ phase-2/05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,60 +1,85 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
+import Timer from "./components/Timer";
 import ProjectList from "./components/ProjectList";
+import ProjectListItem from "./components/ProjectListItem";
 
-// Deliverable 1: Implement useEffect in App component 
+// Deliverable 1: Implement useEffect in App component
 // to load projects
 
-  // Import the `useEffect` hook from the React library
+// Import the `useEffect` hook from the React library
 
-  // Invoke `useEffect` and make a `GET` request using 
-  // the `fetch` method
+// Invoke `useEffect` and make a `GET` request using
+// the `fetch` method
 
-  // Update `projects` state upon successful response 
-  // from the server
+// Update `projects` state upon successful response
+// from the server
 
-// Deliverable 2: Demonstrate the order of operations 
-// between rendering a component and running the side 
+// Deliverable 2: Demonstrate the order of operations
+// between rendering a component and running the side
 // effect
 
-  // Place a console.log() inside the `App` component as 
-  // well as the `useEffect` method
+// Place a console.log() inside the `App` component as
+// well as the `useEffect` method
 
-  // Open up the devtools to observe when each phase of 
-  // the component will occur 
+// Open up the devtools to observe when each phase of
+// the component will occur
 
 const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isTimerShowing, setIsTimerShowing] = useState(true);
+  const [project, setProject] = useState();
+  const [projectId, setProjectId] = useState("");
 
-  const onAddProject = newProject => {
+  useEffect(() => {
+    fetch(`http://localhost:4000/projects/`)
+      .then((r) => r.json())
+      .then((projects) => {
+        setProjects(projects);
+      });
+  }, []);
+
+  const fetchProject = (id) => {
+    fetch(`http://localhost:4000/projects/${id}`)
+      .then((r) => r.json())
+      .then((project) => setProject(project));
+  };
+
+  const handleChange = (e) => {
+    setProjectId(e.target.value);
+    fetchProject(e.target.value);
+  };
+
+  const onAddProject = (newProject) => {
     const newProjectCollection = [...projects, newProject];
     setProjects(newProjectCollection);
-  }
-
-  const handleClick = () => {
-    fetch("http://localhost:4000/projects")
-      .then((res) => res.json())
-      .then((projects) => setProjects(projects));
   };
 
   const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
   // Update "projects" State With Filtered List When Error Occurs
   // With POST Request
-  const onError = filteredList => { setProjects(filteredList) };
+  const onError = (filteredList) => {
+    setProjects(filteredList);
+  };
 
   return (
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-      <ProjectForm 
-        onAddProject={onAddProject} 
+      <button onClick={() => setIsTimerShowing(!isTimerShowing)}>
+        {isTimerShowing ? "Hide Timer" : "Show Timer"}
+      </button>
+      {isTimerShowing ? <Timer /> : null}
+      <ProjectForm
+        onAddProject={onAddProject}
         onError={onError}
         projects={projects}
       />
-      <button onClick={handleClick}>Load Projects</button>
-      <ProjectList projects={projects} />
+      <input value={projectId} onChange={handleChange} />
+      {projectId ? <ProjectListItem {...project} /> : null}
+      {/* <ProjectList projects={projects} /> */}
     </div>
   );
 };
--- phase-2/05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
+++ phase-2/05_side_effects_and_data_fetching/project_showcase/src/components/Timer.js
@@ -1,8 +1,29 @@
-// Deliverable 3: Demonstrate the unmounting and cleanup 
+// Deliverable 3: Demonstrate the unmounting and cleanup
 // phase of a component through `useEffect`
 
-    // Return a cleanup function inside the `useEffect` 
-    // with a console.log()
+// Return a cleanup function inside the `useEffect`
+// with a console.log()
 
-    // Open up the devtools to observe when the cleanup 
-    // will occur during the stages of Component Lifecycle
+// Open up the devtools to observe when the cleanup
+// will occur during the stages of Component Lifecycle
+
+import { useState, useEffect } from "react";
+
+function Timer() {
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    console.log("adding another setInterval");
+    const intervalId = setInterval(() => {
+      setNum((prevNum) => prevNum + 1);
+      console.log("running");
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  return <p>Timer: {num}</p>;
+}
+
+export default Timer;
