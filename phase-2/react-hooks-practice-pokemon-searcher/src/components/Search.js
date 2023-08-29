c071bd2628f16f106111157afd98fdf0817e651f
Morgan VanYperen <morgvanny@gmail.com>
2023-08-29 10:59:06 -0700

    init searcher

---
+++ phase-2/react-hooks-practice-pokemon-searcher/src/components/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div className="ui search">
+      <div className="ui icon input">
+        <input className="prompt" />
+        <i className="search icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Search;
