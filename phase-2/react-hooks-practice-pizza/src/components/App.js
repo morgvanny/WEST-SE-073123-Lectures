9e5b956199a3acbc236d410881d2fc77dcaddc4e
Morgan VanYperen <morgvanny@gmail.com>
2023-09-07 09:15:20 -0700

    init pizza

---
+++ phase-2/react-hooks-practice-pizza/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import PizzaForm from "./PizzaForm";
+import PizzaList from "./PizzaList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <PizzaForm />
+      <PizzaList />
+    </>
+  );
+}
+
+export default App;
---
+++ phase-2/react-hooks-practice-pizza/src/components/PizzaForm.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+function PizzaForm() {
+  return (
+    <form onSubmit={null /*handle that submit*/}>
+      <div className="form-row">
+        <div className="col-5">
+          <input
+            className="form-control"
+            type="text"
+            name="topping"
+            placeholder="Pizza Topping"
+          />
+        </div>
+        <div className="col">
+          <select className="form-control" name="size">
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
+          </select>
+        </div>
+        <div className="col">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="vegetarian"
+              value="Vegetarian"
+            />
+            <label className="form-check-label">Vegetarian</label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="vegetarian"
+              value="Not Vegetarian"
+            />
+            <label className="form-check-label">Not Vegetarian</label>
+          </div>
+        </div>
+        <div className="col">
+          <button type="submit" className="btn btn-success">
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default PizzaForm;
