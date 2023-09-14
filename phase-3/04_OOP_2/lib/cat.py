e37a09b395993c0daba7889564976f9d25e1d666
Morgan VanYperen <morgvanny@gmail.com>
2023-09-14 10:01:57 -0700

    init oop2

---
+++ phase-3/04_OOP_2/lib/cat.py
@@ -0,0 +1,22 @@
+import ipdb
+
+# 7. ✅. Create a subclass of Pet called Cat
+    
+    # import Pet from lib.pet
+
+# 8. ✅. Create Cat class + __init__ that takes all the parameters from Pet and an
+# additional parameter called indoor (Boolean)
+
+    # Use super to pass the Pet parameters to the super class
+
+    # Add an indoor attribute
+
+# ipdb.set_trace()
+
+# 9. ✅. Create a method unique to the Cat subclass called talk which returns the string "Meow!"
+
+# ipdb.set_trace()
+
+# 10. ✅. Create a method called print_pet_details to match the print_pet_details in Pet    
+        
+        # Add super().print_pet_details() and print the indoor attribute
