c1442972bff0f0cec866f72cb20d409769c99051
Morgan VanYperen <morgvanny@gmail.com>
2023-09-14 10:01:29 -0700

    done

--- phase-3/python-p3-anagram-detector-lab/lib/anagram.py
+++ phase-3/python-p3-anagram-detector-lab/lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, original_word):
+        self.original_word_sorted = sorted(original_word.lower())
+
+    def match(self, words):
+        # anagrams = []
+        # for potential_anagram in words:
+        #     if (self.is_anagram(potential_anagram)):
+        #         anagrams.append(potential_anagram)
+        # return anagrams
+        return [word for word in words if self.is_anagram(word.lower())]
+
+    def is_anagram(self, word):
+        return sorted(word) == self.original_word_sorted
