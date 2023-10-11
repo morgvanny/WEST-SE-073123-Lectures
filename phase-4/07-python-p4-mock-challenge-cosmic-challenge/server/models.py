f4e4cba61607b779da9606edbd4308ef113d89b6
Morgan VanYperen <morgvanny@gmail.com>
2023-10-11 09:31:06 -0700

    init cosmic travel

---
+++ phase-4/07-python-p4-mock-challenge-cosmic-challenge/server/models.py
@@ -0,0 +1,60 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Planet(db.Model, SerializerMixin):
+    __tablename__ = 'planets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    distance_from_earth = db.Column(db.Integer)
+    nearest_star = db.Column(db.String)
+
+    # Add relationship
+
+    # Add serialization rules
+
+
+class Scientist(db.Model, SerializerMixin):
+    __tablename__ = 'scientists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+
+    # Add relationship
+
+    # Add serialization rules
+
+    # Add validation
+
+
+class Mission(db.Model, SerializerMixin):
+    __tablename__ = 'missions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    # Add relationships
+
+    # Add serialization rules
+
+    # Add validation
+
+
+# add any models you may need.
