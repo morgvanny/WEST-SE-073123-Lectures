0c9afaaaa486774c18018acf636404580dfa5f20
Morgan VanYperen <morgvanny@gmail.com>
2023-10-13 15:23:41 -0700

    cookies + session

--- phase-4/08-auth/server/app.py
+++ phase-4/08-auth/server/app.py
@@ -1,5 +1,8 @@
+from os import environ
+
 from cast_member_resources import CastMemberResource, CastMembersResource
-from flask import Flask, make_response, request
+from dotenv import load_dotenv
+from flask import Flask, make_response, request, session
 from flask_migrate import Migrate
 from flask_restful import Api
 from models import Production, db
@@ -10,6 +13,48 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 migrate = Migrate(app, db)
 db.init_app(app)
+load_dotenv(".env")
+app.secret_key = environ.get("SECRET_KEY")
+
+
+@app.route("/set_user", methods=["GET"])
+def set_user():
+    # find the user from username
+    # check if password is valid
+    # if so, set user_id to that user's id
+    session["user_id"] = 2
+    return make_response("signed in as user # 2", 200)
+
+
+@app.route("/me", methods=["GET"])
+def me():
+    return make_response("user # " + request.cookies.get("user_id"), 200)
+
+
+@app.route("/set_red", methods=["GET"])
+def set_red():
+    res = make_response("setting cookie to red", 200)
+    res.set_cookie("color", value="red")
+    return res
+
+
+@app.route("/set_blue", methods=["GET"])
+def set_blue():
+    res = make_response("setting cookie to blue", 200)
+    res.set_cookie("color", value="blue")
+    return res
+
+
+@app.route("/set_green", methods=["GET"])
+def set_green():
+    res = make_response("setting cookie to green", 200)
+    res.set_cookie("color", value="green")
+    return res
+
+
+@app.route("/cookie_color", methods=["GET"])
+def cookie_color():
+    return make_response(request.cookies.get("color"), 200)
 
 
 @app.route("/productions", methods=["GET"])
